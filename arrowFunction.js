308b62f006f853486a7b03626c01b3b5b13d7a43
anu-deepa <anudeepakct@gmail.com>
2022-12-29 14:12:59 +0530

    arrow function

---
+++ arrowFunction.js
@@ -0,0 +1,12 @@
+const nameAge = (name, age) => {
+    console.log ("Welcome " + name);
+    console.log (age);
+}
+nameAge('Anu', 123);
+
+
+
+const greetings = () => {
+    console.log("Welcome to JS learnings")
+}
+greetings();
